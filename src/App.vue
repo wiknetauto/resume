4ffc59706ce6f07857aecc070370ef57229b499a
Nathan Wikman <94276090+wiknetauto@users.noreply.github.com>
2023-12-06 10:38:10 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-cover bg-center relative my-background">
+        <div class="w-10/12 p-6 bg-white bg-opacity-75">
+            <div class="flex justify-start items-center pt-4 mt-20 mb-20">
+                <img class="rounded-full object-cover w-24 h-24 max-w-xs" src="./img/nw.jpeg" alt="nathan-wikman">
+                <div class="mt-4 mb-4 ml-4">
+                    <h1 class="text-4xl lg:text-6xl font-bold tracking-wider">Nathan Wikman</h1>
+                    <p class="text-2xl lg:text-3xl mt-2 tracking-wide">Solutions Architect &#64; Integris</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
